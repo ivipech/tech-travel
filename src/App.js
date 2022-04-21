647a78c5445625a0474206ae3a05a8e58e58e9c7
ivipech <ivipech@gmail.com>
2022-04-21 17:27:02 -0500

    cambios en keys y listas

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { ListOption } from "./components";
 
-const customStyle = (backColor = "#909") => ({
-  backgroundColor: backColor,
-  margin: "20px 30px",
-});
-
 function App() {
+  const dias = [
+    { id: 1, nombre: "Lunes" },
+    { id: 2, nombre: "Martes" },
+    { id: 3, nombre: "Miercoles" },
+    { id: 4, nombre: "Jueves" },
+    { id: 5, nombre: "Viernes" },
+    { id: 6, nombre: "Sabado" },
+    { id: 7, nombre: "Domingo" },
+  ];
+
   return (
     <ul>
-      <ListOption className="blue" tiempo="frio">
-        Lunes
-      </ListOption>
-      <ListOption>Martes</ListOption>
-      <ListOption>Miercoles</ListOption>
+      {dias.map((day) => (
+        <ListOption key={day.id}>{day.nombre}</ListOption>
+      ))}
     </ul>
   );
 }
--- src/components/ListOption/index.js
+++ src/components/ListOption/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./style.css";
 
+const customStyle = (backColor = "#9090ff") => ({
+  backgroundColor: backColor,
+  margin: "20px 30px",
+});
+
 const ListOption = (props) => {
   console.log(props);
   return (
-    <li {...props}>
+    <li style={customStyle()}>
       {props.children} {props.tiempo}
     </li>
   );
