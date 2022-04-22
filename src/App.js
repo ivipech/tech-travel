dc7f4b9183388d8d03fbc8375458c74bff56c2fd
ivipech <ivipech@gmail.com>
2022-04-22 11:32:26 -0500

    Render 0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import { ListOption } from "./components";
+import { Timer } from "./components";
 
 function App() {
-  const dias = [
-    { id: 1, nombre: "Lunes" },
-    { id: 2, nombre: "Martes" },
-    { id: 3, nombre: "Miercoles" },
-    { id: 4, nombre: "Jueves" },
-    { id: 5, nombre: "Viernes" },
-    { id: 6, nombre: "Sabado" },
-    { id: 7, nombre: "Domingo" },
-  ];
-
-  return (
-    <ul>
-      {dias.map((day) => (
-        <ListOption key={day.id}>{day.nombre}</ListOption>
-      ))}
-    </ul>
-  );
+  return <Timer />;
 }
 
 export default App;
